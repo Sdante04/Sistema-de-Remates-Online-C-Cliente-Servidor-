f1d884d01d72dd89bb9b7a24ca837c6b723b9863
santi <santiagosimoist@gmail.com>
2025-04-11 17:42:51 -0300

    Creacion de carpeta Config dentro de Common, dentro de esta creacion de SettingsManager.cs, ClientConfig.cs y ServerConfig.cs

---
+++ Common/Config/ClientConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.Config
+{
+    public static class ClientConfig
+    {
+        public static string IPServidor => "127.0.0.1";
+        public static int PuertoServidor => 5000;
+    }
+}
---
+++ Common/Config/SettingsManager.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Config
+{
+    public static class SettingsManager
+    {
+        public static void Cargar()
+        {
+            // Agregar en caso de ser necesario.
+        }
+    }
+}
