73d345812b2ee01f3ccdae2373959fa4ff47a3b4
santi <santiagosimoist@gmail.com>
2025-04-11 17:39:02 -0300

    Creacion de Common, dentro NetworkHelper.cs, FileStreamHelper.cs, Protocolo.cs

---
+++ Common/FileStreamHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace Common
+{
+    public class FileStreamHelper
+    {
+        public byte[] Read(string path, long offset, int length)
+        {
+            byte[] data = new byte[length];
+            using var fs = new FileStream(path, FileMode.Open);
+            fs.Position = offset;
+            int bytesRead = 0;
+            while (bytesRead < length)
+            {
+                int read = fs.Read(data, bytesRead, length - bytesRead);
+                if (read == 0) throw new Exception("No se pudo leer el archivo");
+                bytesRead += read;
+            }
+            return data;
+        }
+
+        public void Write(string fileName, byte[] data)
+        {
+            using var fs = new FileStream(fileName, FileMode.Append);
+            fs.Write(data, 0, data.Length);
+        }
+    }
+}
---
+++ Common/NetworkHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Sockets;
+
+namespace Common
+{
+    public class NetworkHelper
+    {
+        private readonly Socket _socket;
+
+        public NetworkHelper(Socket socket) => _socket = socket;
+
+        public void Send(byte[] data)
+        {
+            int offset = 0;
+            while (offset < data.Length)
+            {
+                int sent = _socket.Send(data, offset, data.Length - offset, SocketFlags.None);
+                if (sent == 0) throw new SocketException();
+                offset += sent;
+            }
+        }
+
+        public byte[] Receive(int dataLength)
+        {
+            byte[] buffer = new byte[dataLength];
+            int offset = 0;
+            while (offset < dataLength)
+            {
+                int received = _socket.Receive(buffer, offset, dataLength - offset, SocketFlags.None);
+                if (received == 0) throw new SocketException();
+                offset += received;
+            }
+            return buffer;
+        }
+    }
+}
