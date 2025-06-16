50b69961deb809ce6a8bf6ac98e23d442e7155a4
Santiago Dante <santiagodante4@gmail.com>
2025-06-16 16:23:21 -0300

    Creacion de clases abstractas de eventos

---
+++ Common/Models/EventoBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Common.Models
+{
+    public abstract class EventoBase
+    {
+        public string Tipo { get; set; }    
+        public DateTime Fecha { get; set; }    
+    }
+}
---
+++ Common/Models/EventoUsuario.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Common.Models
+{
+    public class EventoUsuario : EventoBase
+    {
+        public string Usuario { get; set; }
+    }
+}
