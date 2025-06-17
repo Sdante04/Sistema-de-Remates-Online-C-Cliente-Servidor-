1a8f00e1edd6a90e71d7d56469e20c7e50986fc1
Santiago Dante <santiagodante4@gmail.com>
2025-06-17 19:16:13 -0300

    Creacion de Logica de Filtro de Eventos

---
+++ ServidorEstadisticas/Servicios/FiltroEventosService.cs
@@ -0,0 +1,41 @@
+﻿using Common.Models;
+
+namespace ServidorEstadisticas.Servicios
+{
+    public static class FiltroEventosService
+    {
+        public static List<EventoBase> FiltrarEventos(string? usuario = null, int? articuloId = null, DateTime? desde = null, DateTime? hasta = null)
+        {
+            var eventosFiltrables = AlmacenEventos.TodosLosEventos
+                .Where(e => e is EventoArticulo || e is EventoOferta || e is EventoRemate)
+                .ToList();
+
+            return eventosFiltrables.Where(e =>
+            {
+                bool cumple = true;
+
+                if (!string.IsNullOrWhiteSpace(usuario))
+                {
+                    if (e is EventoArticulo ea && ea.Usuario != usuario) cumple = false;
+                    else if (e is EventoOferta eo && eo.Usuario != usuario) cumple = false;
+                    else if (e is EventoRemate er && er.UsuarioGanador != usuario) cumple = false;
+                }
+
+                if (articuloId.HasValue)
+                {
+                    if (e is EventoArticulo ea && ea.ArticuloId != articuloId) cumple = false;
+                    else if (e is EventoOferta eo && eo.ArticuloId != articuloId) cumple = false;
+                    else if (e is EventoRemate er && er.ArticuloId != articuloId) cumple = false;
+                }
+
+                if (desde.HasValue && e.Fecha < desde.Value)
+                    cumple = false;
+
+                if (hasta.HasValue && e.Fecha > hasta.Value)
+                    cumple = false;
+
+                return cumple;
+            }).ToList();
+        }
+    }
+}
