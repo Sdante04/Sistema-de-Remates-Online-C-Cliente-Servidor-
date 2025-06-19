612d3bf065bc8aeeff1a7c4a315f1702a7dbc325
lautaro93 <LR270036@fi365.ort.edu.uy>
2025-06-19 13:32:41 -0300

    Endpoints [Estadisticas/login, Estadisticas/eventos]

---
+++ ServidorEstadisticasRest/Controllers/EstadisticasController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServidorEstadisticasRest.Servicios;
+
+namespace ServidorEstadisticasRest.Controllers
+{
+    [ApiController]
+    [Route("estadisticas")]
+    public class EstadisticasController : ControllerBase
+    {
+        [HttpGet("login")]
+        public IActionResult EstadisticasLogin()
+        {
+            var estadisticas = EstadisticasService.ObtenerEstadisticasDeLogins();
+            return Ok(new { estadisticas });
+        }
+
+        [HttpGet("eventos")]
+        public IActionResult EventosFiltrados([FromQuery] string? usuario, [FromQuery] int? articuloId, [FromQuery] DateTime? desde, [FromQuery] DateTime? hasta)
+        {
+            var eventos = FiltroEventosService.FiltrarEventos(usuario, articuloId, desde, hasta);
+            return Ok(new { eventos });
+        }
+    }
+}
